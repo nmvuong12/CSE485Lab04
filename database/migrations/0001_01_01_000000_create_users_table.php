5e0cc445e39bc54bf9b76e2538a8a67caa9c8221
nmvuong12 <vuong179301@gmail.com>
2024-12-21 17:51:53 +0700

    Cập nhật Migration table users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('password');
             $table->string('email')->unique();
+            $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
