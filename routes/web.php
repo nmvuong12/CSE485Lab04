53a9485aad6b25889222668ce90ad8a3d282cf8b
danhtrancong-hub <danhtrancong2004@gmail.com>
2024-12-22 17:51:47 +0700

    cap nhat routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReaderController;
+
+Route::get('/readers', [ReaderController::class, 'index'])->name('readers.index');
+Route::get('/readers/create', [ReaderController::class, 'create'])->name('readers.create');
+Route::post('/readers/', [ReaderController::class, 'store'])->name('readers.store');
+Route::get('/readers/{id}/edit', [ReaderController::class, 'edit'])->name('readers.edit');
+Route::put('/reader/{id}', [ReaderController::class, 'update'])->name('readers.update');
+Route::delete('/reader/{id}', [ReaderController::class, 'destroy'])->name('readers.destroy');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Định nghĩa route resource cho ReaderController
+Route::resource('Reader', ReaderController::class);
