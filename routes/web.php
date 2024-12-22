4e24f4ff3620390eb7958a20c9ea45e86e6190e6
Hoang <viethoang101004@gmail.com>
2024-12-22 16:33:49 +0700

    Hoàn thành giao diện và chắc chức năng thêm sửa xóa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+Route::post('/data/store', [BookController::class, 'store'])->name('data.store');
+
+
+Route::resource('books', BookController::class);
 
 Route::get('/', function () {
     return view('welcome');
